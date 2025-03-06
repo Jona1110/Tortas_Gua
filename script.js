e5292078e329675ff7a724d081011c559cbe6c07
Jona1110 <jonamanuel456@gmail.com>
2025-03-06 13:00:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 document.querySelector('.menu-toggle').addEventListener('click', function () {
   document.querySelector('.nav-links').classList.toggle('active');
 });
+
+const video = document.getElementById('video-tortas');
+const playButton = document.getElementById('play-button');
+
+// Ocultar el botón si el video está en reproducción automática
+if (!video.paused) {
+  playButton.style.display = 'none';
+}
+
+playButton.addEventListener('click', () => {
+  if (video.paused) {
+    video.play();
+    playButton.style.display = 'none';
+  } else {
+    video.pause();
+  }
+});
+
+video.addEventListener('play', () => {
+  playButton.style.display = 'none';
+});
+
+video.addEventListener('pause', () => {
+  playButton.style.display = 'block';
+});
